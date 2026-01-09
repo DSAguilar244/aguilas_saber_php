3364600884c4b24c1bfb4d0309a050f9f1708e4a
Ms0204 <marilynnaye28@gmail.com>
2026-01-09 12:18:15 -0500

    css-fix

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Route::middleware('api')
-    ->prefix('api')
-    ->group(base_path('routes/api.php'));
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
     }
 }
